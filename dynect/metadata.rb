18d964d2bf3d4a7d2ec02ab881079580a7319cd4
Lisa Hagemann <lhagemann@dyn.com>
2010-06-17 19:29:14 -0400

    remove the old files

--- dynect/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Dyn Inc"
-maintainer_email "lhagemann@dyn.com"
-license          "Apache 2.0"
-description      "Installs/Configures dynect"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
